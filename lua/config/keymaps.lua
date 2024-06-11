84bbb0e11a7691f813be7574eb3af3156725f65e
Herman Bergner <hermanbergner@gmail.com>
2024-06-11 09:09:12 +0200

    Added scrolling behaviour

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -16,3 +16,7 @@ keymap.set('n', '<leader>sv', '<C-w>v', { desc = 'Split window vertical' })
 keymap.set('n', '<leader>sh', '<C-w>s', { desc = 'Split window horisontal' })
 keymap.set('n', '<leader>se', '<C-w>=', { desc = 'Make splits equal size' })
 keymap.set('n', '<leader>sx', '<cmd>close<CR>', { desc = 'Close current window' })
+
+
+keymap.set('n', '<C-u>', '<C-u>zz', { desc = 'Scroll up and keep cursor center' })
+keymap.set('n', '<C-d>', '<C-d>zz', { desc = 'Scroll down and keep cursor center' })
