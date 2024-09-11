f8b2d086fdd7ee365bef758134e553c19a90ac63
Herman Bergner <hermanbergner@gmail.com>
2024-09-11 08:00:44 +0200

    Added filename to lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -11,6 +11,9 @@ return {
         theme = "catppuccin",
       },
       sections = {
+        lualine_a = { "mode" },
+        lualine_b = { "branch", "diff", "diagnostics" },
+        lualine_c = { { "filename", path = 1 } },
         lualine_x = {
           {
             lazy_status.updates,
